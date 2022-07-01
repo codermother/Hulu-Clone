ccae30602fe4a0dd3c29a10f586155215a017748
codermother <ozgecoskun_3@hotmail.com>
2022-07-01 12:16:08 +0300

    Create nav component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -10,8 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-
-      {/* Navbar */}
+      <Nav />
       {/* Results */}
     </div>
   );
